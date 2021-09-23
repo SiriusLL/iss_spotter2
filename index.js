1c964e7dc5725a1e4b3e11128658ce13f569b2f5
SiriusLL <sirius.love.light@gmail.com>
2021-09-22 19:25:15 -0600

    refactor to waterfall callbacks

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
-const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+const {
+  fetchMyIP,
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation,
+} = require("./iss");
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
 
-  console.log("It worked! Returned IP:", ip);
-});
+//   console.log("It worked! Returned IP:", ip);
+// });
 
-fetchCoordsByIP("209.205.79.252", (error, coords) => {
+// fetchCoordsByIP("209.205.79.252", (error, coords) => {
+//   if (error) {
+//     console.log("It didn't work", error);
+//     return;
+//   }
+//   console.log("It worked! Returned Coordinates are: ", coords);
+// });
+
+// fetchISSFlyOverTimes(
+//   { latitude: "50.9129", longitude: "-114.1028" },
+//   (error, passes) => {
+//     if (error) {
+//       console.log("It didn't work", error);
+//       return;
+//     }
+//     console.log("It worked! Returned Coordinates are: ", passes);
+//   }
+// );
+
+const printPassTimes = (passTimes) => {
+  passTimes.map((time) => {
+    // console.log(time.risetime);
+    const datetime = new Date(time.risetime);
+    // console.log(datetime.toUTCString());
+    // datetime.setUTCSeconds(passTimes.risetime);
+    const duration = time.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  });
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     console.log("It didn't work", error);
     return;
   }
-  console.log("It worked! Returned Coordinates are: ", coords);
+  console.log("It worked! Here is the Pirate Treasure: ", passTimes);
+  printPassTimes(passTimes);
 });
--- iss.js
+++ iss.js
@@ -51,9 +51,58 @@ const fetchCoordsByIP = function (ip, callback) {
 
     const coords = JSON.parse(body);
     // console.log("coords", coords);
-    const result = { lattitude: coords.latitude, longitude: coords.longitude };
+    const result = { latitude: coords.latitude, longitude: coords.longitude };
     callback(null, result);
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+const fetchISSFlyOverTimes = function (coords, callback) {
+  // console.log("ccc", typeof coords.latitude, coords.longitude);
+  const ISSUrl = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(ISSUrl, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} wehn fetching ISS pass times. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const passes = JSON.parse(body).response;
+    // console.log("bodyFOT", passes);
+    callback(null, passes);
+  });
+};
+
+const nextISSTimesForMyLocation = function (callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    fetchCoordsByIP(ip, (error, coords) => {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      fetchISSFlyOverTimes(coords, (error, passes) => {
+        if (error) {
+          callback(error, null);
+          return;
+        }
+        callback(null, passes);
+      });
+    });
+  });
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation,
+};
