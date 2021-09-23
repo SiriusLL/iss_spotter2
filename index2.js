eb479d1805da31d6ef98db3652a607f0142b6509
SiriusLL <sirius.love.light@gmail.com>
2021-09-22 20:49:52 -0600

    refactor to use promises

---
+++ index2.js
@@ -0,0 +1,26 @@
+const {
+  fetchMyIP,
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation,
+} = require("./iss_promised");
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then((body) => {
+//     console.log(body);
+//   });
+
+const printPassTimes = (passTimes) => {
+  passTimes.map((time) => {
+    const datetime = new Date(time.risetime);
+    const duration = time.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  });
+};
+
+// call
+nextISSTimesForMyLocation().then((passTimes) => {
+  printPassTimes(passTimes);
+});
